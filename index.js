2e9666f32ea28d52da3cf038c5bde5f27b3b3822
Mike Pirog <mike@kalamuna.com>
2015-02-17 14:20:53 -0800

    #253: Pressflow env ext plugin first pass

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function(app, events) {
+
+  // Events
+  events.on('pre-install-component', function(component, done) {
+
+    // Add in the pressflow magic
+    // @todo: get his to handle variable input about
+    // driver/prefix/name/db/pass/port/etc
+    var pressflowSettings = {
+      databases: {
+        default: {
+          default: {
+            driver: 'mysql',
+            prefix: '',
+            database: 'kalabox',
+            username: 'kalabox',
+            password: '',
+            host: app.domain,
+            port: 3306,
+          }
+        }
+      },
+      conf: {
+        pressflow_smart_start: 1
+      }
+    };
+    component.installOptions.Env.push('PRESSFLOW_SETTINGS=' + JSON.stringify(pressflowSettings));
+
+    done();
+  });
+
+};
